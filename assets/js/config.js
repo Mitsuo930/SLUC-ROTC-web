6971d9185091f32e07fbf288ea26fbc61b66dea3
Mitsuo930 <ashjuhair@gmail.com>
2026-09-16 18:46:30 +0800

    connect Apps Script API to config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,5 +3,5 @@
 // (You get this after deploying Code.gs - see README.md Step 2)
 // ======================================================
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/PASTE_YOUR_DEPLOYMENT_ID_HERE/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwUB06K0cp3IRrrwXiLdU1ha1_Qss6d5bpUVrEV7hthdQ8OUd9dK5sz7oKphhfgM0Q/exec"
 };
